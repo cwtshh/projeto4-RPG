d1c0dca08d60ade1531fd54c2de6000a4d6130f6
cwtshh <gucosta1719@gmail.com>
2023-08-31 19:44:54 -0300

    adding map graphics and functions to read csv and transform them into graphics

--- ZeldaStyleRPG/model/level.py
+++ ZeldaStyleRPG/model/level.py
@@ -1,3 +1,4 @@
+from random import choice
 from typing import Iterable, Union
 import pygame
 from pygame.sprite import AbstractGroup
@@ -5,6 +6,7 @@ from settings import *
 from utils.tile import Tile
 from model.player import Player
 from debug import debug
+from utils.support import *
 
 class Level:
     def __init__(self):
@@ -21,20 +23,38 @@ class Level:
 
     # cria o mapa
     def createMap(self):
-        # passa pelas linhas
-        for rowIndex, row in enumerate(world_map):
-            # passa pelas colunas
-            for columnindex, column in enumerate(row):
-                x = columnindex * tileSize
-                y = rowIndex * tileSize
+        layouts = {
+            'boundary': importCsvLayout(r'projeto4-RPG\ZeldaStyleRPG\map\map_FloorBlocks.csv'),
+            'grass': importCsvLayout(r'projeto4-RPG\ZeldaStyleRPG\map\map_Grass.csv'),
+            'object': importCsvLayout(r'projeto4-RPG\ZeldaStyleRPG\map\map_Objects.csv')
+        }
 
-                if column == 'x':
-                    # captura a posicao da tile
-                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+        graphics = {
+            'grass': importFolder(r'projeto4-RPG\ZeldaStyleRPG\graphics\levelGraphics\grass'),
+            'objects': importFolder(r'projeto4-RPG\ZeldaStyleRPG\graphics\levelGraphics\objects')
+        }
 
-                if column == 'p':
-                    # captura a posicao do player
-                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+        print(graphics)
+
+        for style, layout in layouts.items():
+            for rowIndex, row in enumerate(layout):
+                for columnIndex, column in enumerate(row):
+                    if column != '-1':
+                        x = columnIndex * tileSize
+                        y = rowIndex * tileSize
+
+                        if style == 'boundary':
+                            Tile((x, y), [self.obstacle_sprites], 'invisible')
+
+                        if style == 'grass':
+                            randomGrassImage = choice(graphics['grass'])
+                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'visible', randomGrassImage)
+
+                        if style == 'object':
+                            surface = graphics['objects'][int(column)]
+                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surface)
+        
+        self.player = Player((2000, 1500), [self.visible_sprites], self.obstacle_sprites)
 
 
 
@@ -58,11 +78,19 @@ class YSortCameraGroup(pygame.sprite.Group):
         # offset do grupo
         self.offset = pygame.math.Vector2()
 
+        # cria o chao
+        self.floorSurface = pygame.image.load(r'projeto4-RPG\ZeldaStyleRPG\graphics\levelGraphics\tilemap\ground.png').convert()
+        self.floor_rect = self.floorSurface.get_rect(topleft = (0, 0))
+
     def customDraw(self, player):
 
         # calcula o offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # desenha o chao
+        floor_offset_position = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floorSurface, floor_offset_position)
 
 
         for sprite in sorted(self.sprites(), key= lambda sprite: sprite.rect.centery):
